ecd5578ad15297e2c6c6844ef0cf6ad274cf90b6
Kundan Bhosale <kundanbhosale99@gmail.com>
2023-04-15 17:22:33 +0530

    CI/CD

--- gen_env_vars.sh
+++
@@ -1,7 +0,0 @@
-RED='\033[0;31m'
-NC='\033[0m' # No Color
-
-echo ${RED}Setting Enviornment variables for task-watch repository${NC}
-gh secret set -f .env.production
-
-
--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   swcMinify: true,
-  trailingSlash: true,
 
   compiler: {
     styledComponents: true,
